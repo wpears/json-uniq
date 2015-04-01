4d30e69cf66869525ba93038865d224070a58ea1
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-04-01 19:28:10 -0400

    Using through2 obj

--- test/test.js
+++ test/test.js
@@ -1,7 +1,14 @@
 var request = require('request');
+var through = require('through2');
+var parser = require('JSONStream');
 
 request.post("http://localhost:9200/address/point/_search?pretty=true",{form:'{' + 
   '"query": {"match_all":{}},' +
   '"sort": {"properties.address":{"order":"asc"}},' +
   '"size": 5}'
-  }).pipe(process.stdout);
+  }).pipe(parser.parse('hits.hits.*'))
+    .pipe(through.obj(function(chunk, enc, cb){
+      this.push(JSON.stringify(chunk)+'\n\n');
+      cb();
+    }))
+    .pipe(process.stdout);
